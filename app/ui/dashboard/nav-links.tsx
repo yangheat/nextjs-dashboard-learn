60ec8f9ea644e8108f1d7a513d5d75b289fd7f29
SunPark <yangheat@gmail.com>
2024-02-20 12:38:55 +0900

    Chapter 11 Adding Search and Pagination

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -23,8 +23,7 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname()
-  console.log(pathname)
-  console.log(links)
+  
   return (
     <>
       {links.map((link) => {
